3edb7c40bb2303589b97b3aa28055ba838d7ab0d
peach <banucho.cassamo@gmail.com>
2014-06-06 10:45:21 +0200

    Adicionando o Splash Screen...

--- Inforgest/src/mz/com/peach/inforgest/SplashScreenActivity.java
+++ Inforgest/src/mz/com/peach/inforgest/SplashScreenActivity.java
@@ -3,7 +3,9 @@ package mz.com.peach.inforgest;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,15 +15,28 @@ import android.os.Build;
 
 public class SplashScreenActivity extends Activity {
 
+	private static int SPLACH_TIME_OUT = 3000;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
+		
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+				startActivity(intent);
+				
+				finish();
+			}
+		}, SPLACH_TIME_OUT);
 
-		if (savedInstanceState == null) {
+		/*if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		}*/
 	}
 
 	@Override
